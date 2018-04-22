71f5b81d1d89911fe9ae3b9892cd681b610b0d31
ketewang <ketewang@youyuantech.com>
2018-04-22 12:46:46 +0800

    完成了客户端程序上传的功能

--- src/main/java/com/spjzweb/controller/ClientAppUpdateController.java
+++ src/main/java/com/spjzweb/controller/ClientAppUpdateController.java
@@ -35,7 +35,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @RequestMapping("/ClientAppUpdate")
 public class ClientAppUpdateController {
 
-    //合同录入
+    //客户端程序包上传
     @RequestMapping(value = "/uploadClientAppPackage",method = RequestMethod.POST)
     public String uploadClientAppPackage(HttpServletRequest request, HttpServletResponse response) throws Exception{
         try {
@@ -64,11 +64,60 @@ public class ClientAppUpdateController {
             }
 
             JSONObject json = new JSONObject();
-            json.put("fileUrl", newName);
-
+            json.put("filename", newName);
+            json.put("filesize", file.length());
             json.put("success",true);
             ResponseUtil.write(response, json);
-            System.out.print("uploadContractList成功");
+            System.out.print("uploadClientAppPackage成功");
+            System.out.println("saveDirectory File="+saveDirectory+"/"+newName);
+            System.out.println("file.length()="+file.length());
+        } catch (Exception e) {
+            System.err.println("Exception=" + e.getMessage().toString());
+            System.out.println("Exception=" + e.getMessage().toString());
+            e.printStackTrace();
+            JSONObject json = new JSONObject();
+            json.put("success",false);
+            ResponseUtil.write(response, json);
+        }
+        return null;
+    }
+
+
+
+    //客户端程序包配置文件上传
+    @RequestMapping(value = "/uploadClientAppAutoUpdaterXML",method = RequestMethod.POST)
+    public String uploadClientAppAutoUpdaterXML(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        try {
+            String saveDirectory = request.getSession().getServletContext().getRealPath("/upload/clientapp");
+            File uploadPath = new File(saveDirectory);
+            if (!uploadPath.exists()) {
+                uploadPath.mkdirs();
+            }
+            //FileRenameUtil util = new FileRenameUtil();
+            System.out.println("saveDirectory="+saveDirectory);
+            MultipartRequest multi = new MultipartRequest(request, saveDirectory, 100* 1024 * 1024, "UTF-8");
+            Enumeration files = multi.getFileNames();
+            String newName = "";
+            File file=null;
+            //HashMap retMap=null;
+            if (files.hasMoreElements()) {
+                String name = (String) files.nextElement();
+
+                file = multi.getFile(name);
+                if (file != null) {
+                    newName = file.getName();
+
+                    //处理excel文件
+                    //retMap =importExcelInfo(saveDirectory+"/"+newName);
+                }
+            }
+
+            JSONObject json = new JSONObject();
+            json.put("filename", newName);
+            json.put("filesize", file.length());
+            json.put("success",true);
+            ResponseUtil.write(response, json);
+            System.out.print("uploadClientAppAutoUpdaterXML成功");
             System.out.println("saveDirectory File="+saveDirectory+"/"+newName);
             System.out.println("file.length()="+file.length());
         } catch (Exception e) {
--- src/main/webapp/system/clientappupdate.jsp
+++ src/main/webapp/system/clientappupdate.jsp
@@ -35,27 +35,52 @@
 </head>
 <body>
 <br />
+
 <div align="left">
     <h3><span class="i18n1" name="plsselectupladclientappfiles">请选择要上传的客户端程序包（.zip）</span></h3>
     <br />
+    <label id="zipfilename" class="hl-label"></label>
+
+    <br/>
     <input id="fileupload1" class="mini-fileupload" name="Fdata" limitType="*.zip;"
            flashUrl="../miniui/fileupload/swfupload/swfupload.swf"
            uploadUrl="/ClientAppUpdate/uploadClientAppPackage.action"
            onuploadsuccess="onUploadSuccess"
            onuploaderror="onUploadError" onfileselect="onFileSelect" width="400px"
     />
-    <br /><br />
 
     <a class="mini-button mini-button-success" width="100px" value="上传" onclick="startUpload()"><span class="i18n1" name="upload">上传</span></a>
 
 </div>
+
+<div align="left">
+    <h3><span class="i18n1" name="plsselectuploadclientappAutoUpdaterXML">请选择要上传的客户端升级配置文件AutoUpdater（.xml）</span></h3>
+    <br />
+    <label id="xmlfilename" class="hl-label"></label>
+
+    <br/>
+    <input id="fileupload2" class="mini-fileupload" name="Fdata" limitType="*.xml;"
+           flashUrl="../miniui/fileupload/swfupload/swfupload.swf"
+           uploadUrl="/ClientAppUpdate/uploadClientAppAutoUpdaterXML.action"
+           onuploadsuccess="onUploadSuccess"
+           onuploaderror="onUploadError" onfileselect="onFileSelect" width="400px"
+    />
+
+    <a class="mini-button mini-button-success" width="100px" value="上传" onclick="startUploadXML()"><span class="i18n1" name="upload">上传</span></a>
+
+</div>
+
+
+
+
+
 <div class="description">
     <h3><span class="i18n1" name="description">描述</span></h3>
     <p><span class="i18n1" name="upgradeinfo">更新信息</span></p>
     <p><a href="../upload/clientapp/clientapp.zip" >点击下载：客户端程序压缩包.zip</a></p>
 </div>
 
-
+<script type="text/javascript" src="../easyui/jquery.easyui.min.js"></script>
 </body>
 </html>
 <script type="text/javascript">
@@ -73,7 +98,17 @@
         //alert("上传成功：" + e.serverData);
         var result = eval('('+e.serverData+')');
         if(result.success){
-            alert("成功上传客户端程序包");
+            var filename=result.filename;
+
+            if(filename.indexOf("zip")>0){
+                $('#zipfilename').text("成功上传："+filename+" 文件大小:"+result.filesize+"bytes");
+            }else{
+                $('#xmlfilename').text("成功上传："+filename+" 文件大小:"+result.filesize+"bytes");
+            }
+            alert("成功上传："+filename+" 文件大小："+result.filesize);
+        }
+        else{
+            alert("成功失败"+result.message);
         }
 
         this.setText("");
@@ -85,6 +120,12 @@
     function startUpload() {
         var fileupload = mini.get("fileupload1");
         fileupload.setUploadUrl("/ClientAppUpdate/uploadClientAppPackage.action");
+        fileupload.startUpload();
+    }
+
+    function startUploadXML() {
+        var fileupload = mini.get("fileupload2");
+        fileupload.setUploadUrl("/ClientAppUpdate/uploadClientAppAutoUpdaterXML.action");
         fileupload.startUpload();
     }
 
