e11e1ef596654e71d92e4242fce6f75649413aa3
ketewang <ketewang@youyuantech.com>
2018-04-23 15:50:22 +0800

    增加了db.properties的读取类

---
+++ src/main/java/com/spjzweb/util/PropertyReader.java
@@ -0,0 +1,45 @@
+package com.spjzweb.util;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+//读取db.properties的配置信息
+
+
+
+
+
+public class PropertyReader {
+    static private String ftpServerAddress = null;
+
+    static{
+        loads();
+    }
+    synchronized static public void loads(){
+        if(ftpServerAddress == null)
+        {
+            InputStream is = PropertyReader.class.getResourceAsStream("/db.properties");
+            Properties dbProps = new Properties();
+            try {
+                dbProps.load(is);
+                ftpServerAddress = dbProps.getProperty("ftpServerAddress");
+            }
+            catch (Exception e) {
+                System.err.println("不能读取属性文件. " +
+                        "请确保db.properties在CLASSPATH指定的路径中");
+            }
+        }
+    }
+    public static String getFtpServerAddress() {
+        if(ftpServerAddress==null)
+            loads();
+        return ftpServerAddress;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("ftp:"+PropertyReader.getFtpServerAddress());
+    }
+
+}
+
